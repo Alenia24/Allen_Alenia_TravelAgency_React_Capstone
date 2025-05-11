6002e9922878d202bd28ec133eb70bb088e1bd23
Alenia24 <alenia.allenn@gmail.com>
2025-05-11 12:02:58 -0400

    Import react routter dom and wrap app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// Import BrowerRouter and rename to Router
+import { BrowserRouter as Router } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
+);
