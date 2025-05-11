4f55c73afe4236f60a59fb0bfe35eef672b8b41d
Alenia24 <alenia.allenn@gmail.com>
2025-05-11 14:14:06 -0400

    Update Navigation component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function Navigation() {
   return (
-    <div>Navigation</div>
-  )
+    <div>
+      <div className="message">
+        <p>
+          Curated Journeys to the World's Most Captivating Destination. Travel
+          Beyond Expectation. <span>Book Now</span>
+        </p>
+      </div>
+
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid className="mx-3 p-0">
+          <Navbar.Brand href="#home">
+            <img
+              src="../../src/assets/logo.png"
+              alt=""
+              width={100}
+              height={50}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to={"/destinations"}>Destinations</Nav.Link>
+              <Nav.Link as={Link} to={"/vacation-types"}>Vacation Types</Nav.Link>
+              <Nav.Link as={Link} to={"/about-us"}>About Us</Nav.Link>
+              <Nav.Link as={Link} to={"/admin-dashboard"}>Admin Dashboard</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
