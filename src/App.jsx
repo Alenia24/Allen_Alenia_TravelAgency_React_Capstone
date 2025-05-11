70e34ae18f41c4b4133f89d32182424c4192c3c7
Alenia24 <alenia.allenn@gmail.com>
2025-05-11 14:30:24 -0400

    Add destinations route

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 // Import Pages
 import HomePage from "./pages/HomePage/HomePage";
+import Destination from "./pages/Destinations/Destination";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/destinations" element={<Destination />} />
         {/* <Route path="*" element={<NoFound />} /> */}
       </Routes>
     </div>
