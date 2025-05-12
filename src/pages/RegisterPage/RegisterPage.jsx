93bdb16897776ea4f3fd81e96c8157b721d99463
Alenia24 <alenia.allenn@gmail.com>
2025-05-12 10:58:41 -0400

    Import register component

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navigation from '../../components/Navigation/Navigation'
 import Footer from '../../components/Footer/Footer'
+import Register from '../../components/Register/Register'
 
 export default function RegisterPage() {
   return (
     <div>
         <Navigation />
+        <Register />
         <Footer />
     </div>
   )
