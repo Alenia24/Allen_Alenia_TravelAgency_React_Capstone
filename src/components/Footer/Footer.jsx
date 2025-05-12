3a86a987921197bf7ca1d1a6d9387b59711dfef6
Alenia24 <alenia.allenn@gmail.com>
2025-05-11 23:21:42 -0400

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
+import React from "react";
+
+import "./Footer.css";
 
 export default function Footer() {
   return (
     <div>
-        <div>
-            <div className="socials">
-                <p>Linkedln</p>
-                <p>Github</p>
-            </div>
-            <div className="site-links">
-                <p>Destinations</p>
-                <p>Vacation Types</p>
-                <p>About Us</p>
-                <p>Contact Us</p>
-            </div>
+      <div className="footer">
+        <div className="footer-info">
+          <div className="socials">
+            <h6>SOCIALS</h6>
+            <p>Linkedln</p>
+            <p>Github</p>
+          </div>
+          <div className="site-links">
+            <h6>SOCIAL LINKS</h6>
+            <p>Destinations</p>
+            <p>Vacation Types</p>
+            <p>About Us</p>
+            <p>Contact Us</p>
+          </div>
 
-            <div className="contact-info">
-                <p>alenia.allenn@gmail.com</p>
-            </div>
+          <div className="contact-info">
+            <h6>CONTACT INFO</h6>
+            <p>alenia.allenn@gmail.com</p>
+          </div>
 
-            <div className="legal-info">
-                <p>Privacy Policies</p>
-                <p>Terms & Conditions</p>
-                <p>Travel Disclosures or Insurance Info</p>
-            </div>
+          <div className="legal-info">
+            <h6>LEGAL INFO</h6>
+            <p>Privacy Policies</p>
+            <p>Terms & Conditions</p>
+            <p>Travel Disclosures or Insurance Info</p>
+          </div>
         </div>
+
+        <div className="copyright">© 2025 Solara Escapes</div>
+      </div>
     </div>
-  )
+  );
 }
