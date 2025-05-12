26e30e72bf22ad8d7038b1e42b14d563640affcb
Alenia24 <alenia.allenn@gmail.com>
2025-05-12 16:44:11 -0400

    Udpade and design login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,37 +2,63 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { postLogin, postRegister } from "../../services/travel-api";
 
+import "./Login.css";
+
 export default function Register() {
   const nav = useNavigate();
   const createLogin = (e) => {
     e.preventDefault();
     const loginData = {
       username: e.target.username.value,
-      password: e.target.username.value,
+      password: e.target.password.value,
     };
 
-    postLogin(loginData).then ((user) => {
-        if(user.role ==="admin") {
-          nav("/admindashoard")  
-        } else {
-            nav("/")
-        }
-        
-    })
+    postLogin(loginData).then((user) => {
+      if (user.role === "admin") {
+        nav("/admindashboard");
+      } else {
+        nav("/");
+      }
+    });
+  };
+  const handleNavigation = () => {
+    nav("/register");
   };
 
   return (
-    <div>
-      <div>
-        <h1>Login</h1>
-        <form onSubmit={createLogin} method="post">
-          <label htmlFor="username">Username</label> <br />
-          <input type="text" name="username" id="username" /> <br />
-          <label htmlFor="password">Password</label> <br />
-          <input type="password" name="password" id="password" required />
-          <br />
-          <button type="submit">Submit</button>
+    <div className="login-container">
+      <div className="login">
+        <img
+          className="img-fluid"
+          src="../../src/assets/logo.png"
+          alt="solara escapes logo"
+          width={100}
+        />
+        <h1>Welcome Back!</h1>
+        <h2>Login</h2>
+        <form className="login-form" onSubmit={createLogin} method="post">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            required
+            placeholder="Enter username..."
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+            placeholder="Enter password..."
+          />
+
+          <button type="submit">Log In</button>
         </form>
+        <p>
+          Don't have an account. <button className="signup-btn" onClick={handleNavigation}>Sign Up</button>
+        </p>
       </div>
     </div>
   );
