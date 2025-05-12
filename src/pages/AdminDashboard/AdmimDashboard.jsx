4288d945fb3d41dd8c4f1695b5315edfd09d5338
Alenia24 <alenia.allenn@gmail.com>
2025-05-12 14:20:21 -0400

    Create admin page

---
+++ src/pages/AdminDashboard/AdmimDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "../../components/Navigation/Navigation";
+import Footer from "../../components/Footer/Footer";
+
+export default function AdmimDashboard() {
+  return (
+    <div>
+      <Navigation />
+      <Footer />
+    </div>
+  );
+}
