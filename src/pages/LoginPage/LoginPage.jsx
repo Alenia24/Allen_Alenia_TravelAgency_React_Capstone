99a919f612ae64a5c0922c2dc920830435578fd5
Alenia24 <alenia.allenn@gmail.com>
2025-05-12 14:15:59 -0400

    Import login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Navigation from '../../components/Navigation/Navigation'
+import Footer from '../../components/Footer/Footer'
+import Login from '../../components/Login/Login'
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div>
+      <Navigation/>
+      <Login />
+      <Footer />
+    </div>
   )
 }
