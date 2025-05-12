e5763956b45c263023daff602731f361cf1e25b3
Alenia24 <alenia.allenn@gmail.com>
2025-05-11 22:53:25 -0400

    Create Server

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <div>
+            <div className="socials">
+                <p>Linkedln</p>
+                <p>Github</p>
+            </div>
+            <div className="site-links">
+                <p>Destinations</p>
+                <p>Vacation Types</p>
+                <p>About Us</p>
+                <p>Contact Us</p>
+            </div>
+
+            <div className="contact-info">
+                <p>alenia.allenn@gmail.com</p>
+            </div>
+
+            <div className="legal-info">
+                <p>Privacy Policies</p>
+                <p>Terms & Conditions</p>
+                <p>Travel Disclosures or Insurance Info</p>
+            </div>
+        </div>
+    </div>
+  )
+}
