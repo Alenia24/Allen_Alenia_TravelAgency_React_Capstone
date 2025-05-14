04df26ed9102aed92477c266028852077cc0ea66
Alenia24 <alenia.allenn@gmail.com>
2025-05-13 22:58:36 -0400

    Update and redesign

--- src/components/AllDestinations/AllDestinations.jsx
+++ src/components/AllDestinations/AllDestinations.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
+import "./AllDestinations.css"
+
 export default function AllDestinations({ trips }) {
   return (
     <div>
-      <h1 className="title">Destinations</h1>
-      <div className="destinations">
+      <h1 className="all-destinations-title">Destinations</h1>
+      <div className="all-destinations">
         {trips.map((trip) => {
           return (
             <Link to={`/destinations/${trip._id}`}>
-              <div className="card">
-                <div className="image">
+              <div className="all-destinations-card">
+                <div className="all-destinations-image">
                   <img
                     className="img-fluid"
                     src={trip.imageURL}
